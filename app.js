722c8696f1fbda96552b00ae0bcb8eed9c3647ce
Orleandro Lincoln <76212175+Vornak@users.noreply.github.com>
2023-10-29 11:46:15 -0300

    tirando-prevent-defult

--- app.js
+++ app.js
@@ -67,50 +67,21 @@ function consulta(){
   
 }
 
-// Adicione ouvintes de evento para cada elemento
-elementoLugar.addEventListener('click', function(event) {
-  event.preventDefault();
-  setTimeout(function() {
-    remove(lugar, 1, "lugarF");
-  }, 2000);
-});
+setTimeout(function() {
+  remove(lugar, 1, "lugarF");
+}, 2000);
 
-elementoFilhos.addEventListener('click', function(event) {
-  event.preventDefault();
-  setTimeout(function() {
-    remove(filhos, 1, "filhoF");
-  }, 3000);
-});
+setTimeout(function() {
+  remove(filhos, 1, "filhoF");
+}, 3000);
 
-elementoDinheiro.addEventListener('click', function(event) {
-  event.preventDefault();
-  setTimeout(function() {
-    remove(dinheiro, 1, "dinheiroF");
-  }, 4000);
-});
+setTimeout(function() {
+  remove(dinheiro, 1, "dinheiroF");
+}, 4000);
 
-elementoCasamento.addEventListener('click', function(event) {
-  event.preventDefault();
-  setTimeout(function() {
-    remove(casamento, 1, "casamentoF");
-  }, 5000);
-});
-
-// setTimeout(function() {
-//   remove(lugar, 1, "lugarF");
-// }, 2000);
-
-// setTimeout(function() {
-//   remove(filhos, 1, "filhoF");
-// }, 3000);
-
-// setTimeout(function() {
-//   remove(dinheiro, 1, "dinheiroF");
-// }, 4000);
-
-// setTimeout(function() {
-//   remove(casamento, 1, "casamentoF");
-// }, 5000);
+setTimeout(function() {
+  remove(casamento, 1, "casamentoF");
+}, 5000);
 
 
 }
