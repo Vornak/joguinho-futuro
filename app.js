98f5689e5e8dcb71fe2cb54fc6620dc0e51f1a97
Orleandro Lincoln <76212175+Vornak@users.noreply.github.com>
2023-10-29 11:34:01 -0300

    versao-funcional1

--- app.js
+++ app.js
@@ -14,72 +14,78 @@ function calcula(){
     const filhos = ['0', '1', '2', '4', '5']
     const lugar = [lugar1, lugar2, lugar3]
 
-
+    const enrolando = document.getElementById("enrolando")
 
     idade = parseInt(document.getElementById("idade").value) 
 
-    function remove(arr, modificador){
-        for (let i = 0; i < idade; i++) {
-            const index1 = i % arr.length;
-            const index2 = i % arr.length-1;
-            if(index2 === undefined){
-              index2 = i % arr.length
-            }
-            // const el = arr[index];
-            // console.log(`Elemento ${i + 1}: ${el}`);
+    function remove(arr, modificador, el){
 
-            c = document.getElementById("encheLinguica")
-            l = document.getElementById("encheLinguica2")
-        
-            if (i === idade - 1) {
-              if(modificador==1){
-                let newArr = arr[index1]; 
-                console.log(newArr);
-                return c.innerHTML = newArr
-              }else if(modificador==2){
-                let newArr = arr[index2]; 
-                console.log(newArr);
-                return c.innerHTML = newArr
-              }else if(modificador==3){
-                let newArr = arr[index1]; 
-                console.log(newArr);
-                return c.innerHTML = newArr
-              }else if(modificador==4){
-                let newArr = arr[index2]; 
-                console.log(newArr);
-                return c.innerHTML = newArr
-              }
-            }
-          }
-    }
+      elemento = document.getElementById(el)
+      console.log(elemento)
+      
 
-    // setTimeout(remove(casamento, 1, ), 5000)
-    // setTimeout(remove(lugar,2), 5000)
+      for (let i = 0; i < idade; i++) {
+        const index1 = i % arr.length;
+        const index2 = i % arr.length-1;
+        if(index2 === undefined){
+          index1 = i % arr.length
+        }
+        if(index2 ===undefined){
+          index2 = i % arr.length
+        }
 
-    // setTimeout(remove(dinheiro,3), 5000)
-    // setTimeout(remove(filhos,4), 5000)
-
-    remove(casamento, 1)
-    remove(lugar,2)
-    remove(dinheiro,3)
-    remove(filhos,4)
-
+        // const el = arr[index];
+        // console.log(`Elemento ${i + 1}: ${el}`);
     
-    teste = document.getElementById("encheLinguica")
+        if (i === idade - 1) {
+          if(modificador==1){
 
+            let newArr = arr[index1]; 
+            console.log(newArr);
+            return elemento.innerHTML = newArr
+
+          }else if(modificador==2){
+
+            let newArr = arr[index2]; 
+            console.log(newArr);
+            return elemento.innerHTML = newArr
+          
+        }
+      }
+  }
+}
+
+function consulta(){
+  let palavras = [
+    "Consultando astros...",
+    "Contactando aliens..."
     
+  ]
 
+  palavraEscolhida = Math.floor(Math.random() * palavras.lenght)
+  toString(palavraEscolhida)
+  // enrolando.innerHTML = palavraEscolhida
   
-  
-  
-  
-
-    
 }
 
 
 
+setTimeout(function() {
+  remove(lugar, 1, "lugarF");
+}, 2000);
+
+setTimeout(function() {
+  remove(filhos, 1, "filhoF");
+}, 3000);
+
+setTimeout(function() {
+  remove(dinheiro, 1, "dinheiroF");
+}, 4000);
+
+setTimeout(function() {
+  remove(casamento, 1, "casamentoF");
+}, 5000);
 
 
-
+}
 
